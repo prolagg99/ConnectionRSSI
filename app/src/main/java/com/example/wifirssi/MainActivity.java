ef57a600c7b8dbb9da0fe36bd8137eec6e834df5
ILiiNe <pro.lagg99@gmail.com>
2021-06-12 20:29:08 +0200

    update the values of RSSI each 2 seconds

--- app/src/main/java/com/example/wifirssi/MainActivity.java
+++ app/src/main/java/com/example/wifirssi/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.wifirssi;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
@@ -10,53 +9,35 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.location.LocationManager;
 import android.net.ConnectivityManager;
-import android.net.DhcpInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
-import java.math.BigInteger;
-import java.net.InetAddress;
-import java.nio.ByteOrder;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    // for wifi list
-    TextView tvWifiList;
     WifiManager wifiManager;
+    private int mInterval = 2000; // 5 seconds by default, can be changed later
+    private Handler mHandler;
 
+    IntentFilter intentFilter = new IntentFilter();
 
     final private int REQUEST_CODE_ASK_PERMISSIONS = 13;
-    TextView tvCellIdCode1, tvWifiSignal, tvRouterIpAddress, tvRouterMacAddress,
-            tvNetworkType, tvCellRSSI, tvCellIDCode2;
-    ImageView ivWifiSignal, ivWifiSignalList;
-    Button btnCellId, wifiRSSIActivity;
-
-
-    ListView listView;
-    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
-    ArrayList<String> listItems=new ArrayList<String>();
-
-    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
-    ArrayAdapter<String> adapter;
-
+    TextView tvWifiSignal, tvRouterMacAddress;
+    ImageView ivWifiSignal;
+    Button  wifiRSSIActivity;
 
     private BroadcastReceiver networkChangeReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             Log.e("app","Network connectivity change");
-
             WifiInfo wifiInfo = wifiManager.getConnectionInfo();
 
             // get SSID
@@ -72,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
             int numberOfLevels = 5;
             int level = WifiManager.compareSignalLevel(wifiInfo.getRssi(), numberOfLevels);
             levelOfSignal(level, ivWifiSignal);
-
         }
     };
 
@@ -81,9 +61,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mHandler = new Handler();
+        startRepeatingTask();
+
         wifiRSSIActivity = (Button) findViewById(R.id.wifiRSSIActivity);
-
-
 
         wifiRSSIActivity.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -109,105 +90,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
-        // for wifi list
-        tvWifiList = (TextView) findViewById(R.id.tvWifiList);
-        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-
-        tvCellIdCode1 = (TextView) findViewById(R.id.tvCellIDCode1);
-        tvWifiSignal = (TextView) findViewById(R.id.tvWifiSignal);
-        tvRouterIpAddress = (TextView) findViewById(R.id.tvRouterIpAddress);
-        tvRouterMacAddress = (TextView) findViewById(R.id.tvRouterMacAddress);
-        ivWifiSignal = (ImageView) findViewById(R.id.ivWifiSignal);
-        btnCellId = (Button) findViewById(R.id.btnCellID);
-
-
-        listView = (ListView) findViewById(R.id.wifiList);
-        adapter =  new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, listItems);
-        listView.setAdapter(adapter);
-
-
-        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        registerReceiver(networkChangeReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-        wifiManager.startScan();
-
-        btnCellId.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.M)
-            @Override
-            public void onClick(View v) {
-
-//                WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-                // for wifi list
-                // check if GPS turned OFF
-                final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
-//                if ( !manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
-//                    buildAlertMessageNoGps();
-//                }
-//                if (mainWifi.isWifiEnabled() == false)
-//                {
-//                    // If wifi disabled then enable it
-//                    Toast.makeText(getApplicationContext(), "wifi is disabled..making it enabled",
-//                            Toast.LENGTH_LONG).show();
-//                    mainWifi.setWifiEnabled(true);
-//                }
-
-
-
-
-
-
-
-                // get the SSID & RSSI & ip_@ of wireless network wifi
-//                WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-
-//                String ssid = wifiInfo.getSSID();
-//                int rssi = wifiInfo.getRssi();
-//                tvWifiSignal.setText(getResources().getString(R.string.wifi_signal, ssid, rssi));
-
-//                Toast.makeText(ConnectivityActivity.this,"SSID " + ssid + " RSSI " + rssi, Toast.LENGTH_LONG).show();
-
-                // get the Mac @ of router
-//                String BSSID = wifiInfo.getBSSID();
-//                Toast.makeText(MainActivity.this,"BSSID " + BSSID, Toast.LENGTH_LONG).show();
-//                tvRouterMacAddress.setText(getResources().getString(R.string.mac_address, BSSID));
-
-                // get the ip_@ of wifi
-//                final DhcpInfo dhcp = wifiManager.getDhcpInfo();
-//                int ipAddress = dhcp.gateway;
-//                ipAddress = (ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN)) ?
-//                        Integer.reverseBytes(ipAddress) : ipAddress;
-//                byte[] ipAddressByte = BigInteger.valueOf(ipAddress).toByteArray();
-//                try {
-//                    InetAddress myAddr = InetAddress.getByAddress(ipAddressByte);
-//                    tvRouterIpAddress.setText(getResources().getString(R.string.ip_address, myAddr.getHostAddress()));
-////                    Toast.makeText(ConnectivityActivity.this,"ip address " + myAddr.getHostAddress(), Toast.LENGTH_LONG).show();
-//                }catch (Exception e){
-//                }
-
-                // get the signal level and display it as icon
-//                int numberOfLevels = 5;
-//                int level = WifiManager.compareSignalLevel(wifiInfo.getRssi(), numberOfLevels);
-//                Toast.makeText(ConnectivityActivity.this,"level " + level, Toast.LENGTH_LONG).show();
-
-//                levelOfSignal(level, ivWifiSignal);
-
-//                Excellent >-50 dBm
-//                Good -50 to -60 dBm
-//                Fair -60 to -70 dBm
-//                Weak < -70 dBm
-
-
-            }
-        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
-        IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
         registerReceiver(networkChangeReceiver, intentFilter);
     }
@@ -215,9 +102,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-
         unregisterReceiver(networkChangeReceiver);
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopRepeatingTask();
+    }
+
+    void startRepeatingTask() {
+        mStatusChecker.run();
+    }
+
+    void stopRepeatingTask() {
+        mHandler.removeCallbacks(mStatusChecker);
+    }
+
+    Runnable mStatusChecker = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+                registerReceiver(networkChangeReceiver, intentFilter);
+            } finally {
+                // 100% guarantee that this always happens, even if
+                // your update method throws an exception
+                mHandler.postDelayed(mStatusChecker, mInterval);
+            }
+        }
+    };
 
     private void levelOfSignal(int level, ImageView ivSignalLevel) {
         if (level <= 0 && level >= -50) {
@@ -238,35 +152,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
 }
-
-//        part[0] = "Signalstrength:"  _ignore this, it's just the title_
-//
-//        parts[1] = GsmSignalStrength
-//
-//        parts[2] = GsmBitErrorRate
-//
-//        parts[3] = CdmaDbm
-//
-//        parts[4] = CdmaEcio
-//
-//        parts[5] = EvdoDbm
-//
-//        parts[6] = EvdoEcio
-//
-//        parts[7] = EvdoSnr
-//
-//        parts[8] = LteSignalStrength
-//
-//        parts[9] = LteRsrp
-//
-//        parts[10] = LteRsrq
-//
-//        parts[11] = LteRssnr
-//
-//        parts[12] = LteCqi
-//
-//        parts[13] = gsm|lte|cdma
-//
-//        parts[14] = _not really sure what this number is_
